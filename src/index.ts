d57a6ae8a19d743505399eaa96bb9219d9d94868
[Flutter_SakuSaku13:44] <[gitonly543@gmail.com]>
2023-01-27 10:21:53 +0900

    add Prettierrc

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,9 @@
 type User = {
-    id: string;
-    name: string;
-    age: number;
+  id: string;
+  name: string;
+  age: number;
 };
-function getUser(): Promise<User> {
-    return new Promise((resolve) => {
-        const user: User = {
-            id: "",
-            name: "太郎",
-            age: 10,
-        };
-        resolve(user);
-    });
-}
 getUser().then((user: User) => {
-    console.log(user);
-    // @log: { "name": "太郎", "age": 10 }
+  console.log(user);
+  // @log: { "name": "太郎", "age": 10 }
 });
